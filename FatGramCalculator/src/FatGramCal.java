cd5ad7d23ff4d89cf5e86e26a6234e051ba3e801
Swatantra Goswami <swatantra.goswami@nagarro.com>
2021-08-23 16:55:59 +0530

    First commit

--- FatGramCalculator/src/FatGramCal.java
+++ FatGramCalculator/src/FatGramCal.java
@@ -1,19 +1,50 @@
 
 //Solution of Assignment-1
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class FatGramCal {
+	
+	 public static boolean validtaeString(String str) {
+	      str = str.toLowerCase();
+	      char[] charArray = str.toCharArray();
+	      for (int i = 0; i < charArray.length; i++) {
+	         char ch = charArray[i];
+	         if (!(ch >= 'a' && ch <= 'z')) {
+	            return false;
+	         }
+	      }
+	      return true;
+	   }
 
 	public static void main(String[] args) {
 //		System.out.println("hello");
 		Scanner sc=new Scanner(System.in);
+		StringBuffer sb=new StringBuffer(); 
 		FatGramCalc fgc= new FatGramCalc();
 		try {
-		System.out.println("Please Enter the number of calories");
+			
+			System.out.println("Enter the name of a food item: ");
+			String food= sc.nextLine();
+			
+			boolean bool = validtaeString(food);
+			
+			if(!bool) {
+		         System.out.println("Given String is invalid");
+		         System.exit(0);
+		      }
+			
+			
+			
+			
+			
+			
+			
+		System.out.println("Enter the number of calories: ");
 		fgc.totalNumberOfCalories = sc.nextDouble();
 
-		System.out.println("Please Enter the number of fat Gram in the food");
+		System.out.println("Enter the grams of fat: ");
 		fgc.fatGrams = sc.nextDouble();
 		
 		fgc.caloriesFromFat = fgc.fatGrams*9;
@@ -33,10 +64,16 @@ public class FatGramCal {
 		}
 		
 		
-		}catch(Exception e) {
-			System.out.println("Please Enter Correct Value");
+		} catch (Exception  e) {
+			  System.out.println("Please Enter Correct Value");
 		}
+
 		
 		
+	}
+
+	private static char[] type(String food) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
